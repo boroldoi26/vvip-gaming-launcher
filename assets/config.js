86e2dcacb93999c0f1d4e3b8aab75b560f1c3f1a
boroldoi26 <62096740+boroldoi26@users.noreply.github.com>
2026-08-10 14:44:33 +0800

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
 // VVIP Gaming Room GitHub Pages frontend -> Apps Script API backend
 // This URL must be the deployed Apps Script Web App URL ending in /exec.
-window.VVIP_API_URL = 'https://script.google.com/macros/s/AKfycbyl6AV0U2wWrYHEQfx8wthMioiJAcL_CKfGx5ODTrddIC28vwRVrikkiS5F5_Du07IX/exec';
+window.VVIP_API_URL = 'https://script.google.com/macros/s/AKfycbziwjV4zgc9A5ai8Gm1lNmHe6V8r0qaWgxhbS6yWVP5t8Ow0pA9n7UQa1q8t2ZaXwX_/exec';
